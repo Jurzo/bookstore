fd4bdeb24d31c11fd584628d089558a84b138671
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-02-11 14:46:56 +0200

    3.2 done

--- src/main/java/fi/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/fi/hh/swd20/bookstore/web/BookController.java
@@ -1,9 +1,14 @@
 package fi.hh.swd20.bookstore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fi.hh.swd20.bookstore.domain.Book;
 import fi.hh.swd20.bookstore.domain.BookRepository;
 
 @Controller
@@ -15,6 +20,13 @@ public class BookController {
     @RequestMapping("/index")
     public String bookstore() {
         return "temp";
+    }
+
+    @GetMapping("/booklist")
+    public String booklist(Model model) {
+        List<Book> books = (List<Book>) repository.findAll();
+        model.addAttribute("books", books);
+        return "booklist";
     }
     
 }
