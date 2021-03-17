2675877525fed7fedb2645f2d7d86fc27ad28fcf
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-03-17 14:15:48 +0200

    user entities 6.2

--- src/main/java/fi/hh/swd20/bookstore/BookstoreApplication.java
+++ src/main/java/fi/hh/swd20/bookstore/BookstoreApplication.java
@@ -11,6 +11,8 @@ import fi.hh.swd20.bookstore.domain.Book;
 import fi.hh.swd20.bookstore.domain.BookRepository;
 import fi.hh.swd20.bookstore.domain.Category;
 import fi.hh.swd20.bookstore.domain.CategoryRepository;
+import fi.hh.swd20.bookstore.domain.User;
+import fi.hh.swd20.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -22,33 +24,30 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-    public CommandLineRunner demo(BookRepository repository, CategoryRepository catRep) {
+    public CommandLineRunner demo(BookRepository books, CategoryRepository categories, UserRepository users) {
         return (args) -> {
 
 			log.info("Save some test categories");
 			Category cat1 = new Category("SciFi");
 			Category cat2 = new Category("Fantasy");
 			Category cat3 = new Category("Education");
-			catRep.save(cat1);
-			catRep.save(cat2);
-			catRep.save(cat3);
-
-			
-			log.info("fetch all categories");
-			for (Category c : catRep.findAll()) {
-				log.info(c.toString());
-			}
+			categories.save(cat1);
+			categories.save(cat2);
+			categories.save(cat3);
 
             log.info("Save some test books");
 			Book book1 = new Book("Harry Potter", "JKRowling", "2000", "1234456", 12.5, cat1);
 			Book book2 = new Book("Testikirja", "Tero Testi", "2009", "1234456", 50.5);
-			repository.save(book1);
-			repository.save(book2);
+			books.save(book1);
+			books.save(book2);
 
-			log.info("fetch all books");
-			for (Book b : repository.findAll()) {
-				log.info(b.toString());
-			}
+			// admin:admin, user:user
+			log.info("Create some users to database");
+			User user = new User("user", "user@domain", "$2a$10$jpCEOlTIWzeGLfCdT9HBmeP8oz9/Z4w3vpaHbFB4JewP/o9v2G6Ji", "USER");
+			User admin = new User("admin", "admin@domain", "$2a$10$BL3dOAcbj0sHZwcIiNSvoO8nyfeVwGdWSMPriJwQo5yOoXhcIPOMe", "ADMIN");
+			users.save(user);
+			users.save(admin);
+
         };
     }
 
